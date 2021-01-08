1b08b17483049bbbad0f9ae8f54818dedcc86989
Lars Mikkelsen <lars@ionic.io>
2021-01-08 13:33:51 -0500

    next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/foo/(.*)",
+        destination: "https://httpbin.org/anything/$1"
+      }
+    ];
+  },
+  trailingSlash: true
+};
